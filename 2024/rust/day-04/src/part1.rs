261ca88fc31ec086ea8648230e19f2c48b59440d
Ole Voß <ole.voss@posteo.de>
2024-12-07 23:36:44 +0100

    solve day4

--- 2024/rust/day-04/src/part1.rs
+++ 2024/rust/day-04/src/part1.rs
@@ -1,156 +1,55 @@
-use itertools::Itertools;
-use tracing::info;
-
 #[tracing::instrument]
 pub fn process(input: &str) -> miette::Result<String> {
-    let mut xmas_hits: Vec<XmasHit> = Vec::new();
+    let mut xmas_count = 0;
 
     let grid = parse_grid(input)?;
-    let mut col_index = 0;
-    while col_index + 4 <= grid[0].len() {
-        let mut row_index = 0;
-        while row_index + 4 <= grid.len() {
-            let square: Vec<[char; 4]> = (0..4)
-                .into_iter()
-                .map(|i| {
-                    let row: [char; 4] = [
-                        grid[row_index + i][col_index],
-                        grid[row_index + i][col_index + 1],
-                        grid[row_index + i][col_index + 2],
-                        grid[row_index + i][col_index + 3],
-                    ];
-                    return row;
-                })
-                .collect();
-            xmas_hits.append(&mut find_in_square(square, row_index, col_index)?);
-            row_index += 1;
+
+    for row in 0..grid.len() {
+        for col in 0..grid[0].len() {
+            if grid[row][col] == 'X' {
+                let hits: u32 = DIRECTIONS
+                    .iter()
+                    .map(|dir_vec| {
+                        let word: String = dir_vec
+                            .iter()
+                            .map(|pos| {
+                                let x = (row as i32) + pos.0;
+                                let y = (col as i32) + pos.1;
+                                match grid.get(x as usize) {
+                                    Some(r) => match r.get(y as usize) {
+                                        Some(c) => c,
+                                        None => &'.',
+                                    },
+                                    None => &'.',
+                                }
+                            })
+                            .collect();
+                        if word == "MAS" {
+                            1
+                        } else {
+                            0
+                        }
+                    })
+                    .sum();
+                xmas_count += hits;
+            }
         }
-        col_index += 1;
     }
-    xmas_hits.sort();
-    xmas_hits.dedup();
-    let xmas_count = xmas_hits.len();
+
     Ok(xmas_count.to_string())
 }
 
 // Patterns
-const XMAS_NORMAL: [char; 4] = ['X', 'M', 'A', 'S'];
-const XMAS_REVERSE: [char; 4] = ['S', 'A', 'M', 'X'];
-
-#[derive(Debug, PartialOrd, Ord)]
-struct XmasHit {
-    row: usize,
-    col: usize,
-    orient: Orientation,
-    direction: Direction,
-}
-
-impl XmasHit {
-    pub fn new(row: usize, col: usize, or: Orientation, dir: Direction) -> Self {
-        Self {
-            row,
-            col,
-            orient: or,
-            direction: dir,
-        }
-    }
-}
-
-impl PartialEq for XmasHit {
-    fn eq(&self, other: &Self) -> bool {
-        self.row == other.row
-            && self.col == other.col
-            && self.orient == other.orient
-            && self.direction == other.direction
-    }
-}
-
-impl Eq for XmasHit {}
-
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
-enum Orientation {
-    Horizontal,
-    Vertical,
-    Diagonal,
-}
-
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
-enum Direction {
-    Normal,
-    Reverse,
-}
-
-fn find_in_square(
-    square: Vec<[char; 4]>,
-    row: usize,
-    column: usize,
-) -> miette::Result<Vec<XmasHit>> {
-    let mut hits: Vec<XmasHit> = Vec::new();
-    dbg!("New finder::\n");
-    dbg!(row, column, &square);
-    for i in 0..4 {
-        // rows
-        if square[i] == XMAS_NORMAL {
-            hits.push(XmasHit::new(
-                row,
-                column,
-                Orientation::Horizontal,
-                Direction::Normal,
-            ));
-        } else if square[i] == XMAS_REVERSE {
-            hits.push(XmasHit::new(
-                row,
-                column,
-                Orientation::Horizontal,
-                Direction::Reverse,
-            ));
-        }
-
-        // cols
-        let col: Vec<char> = square.iter().map(|r| r[i]).collect();
-        if col == XMAS_NORMAL {
-            hits.push(XmasHit::new(
-                row,
-                column,
-                Orientation::Vertical,
-                Direction::Normal,
-            ));
-        } else if col == XMAS_REVERSE {
-            hits.push(XmasHit::new(
-                row,
-                column,
-                Orientation::Vertical,
-                Direction::Reverse,
-            ));
-        }
-    }
-
-    // diagonal
-    for direction in [0, 3] {
-        let dia: Vec<char> = (0..4)
-            .into_iter()
-            .map(|i| square[i][(direction as i32 - i as i32).abs() as usize])
-            .collect();
-
-        if dia.as_slice() == XMAS_NORMAL {
-            hits.push(XmasHit::new(
-                row,
-                column,
-                Orientation::Diagonal,
-                Direction::Normal,
-            ));
-        } else if dia.as_slice() == XMAS_REVERSE {
-            hits.push(XmasHit::new(
-                row,
-                column,
-                Orientation::Diagonal,
-                Direction::Reverse,
-            ));
-        }
-    }
-    dbg!(&hits);
-    Ok(hits)
-}
+const DIRECTIONS: [[(i32, i32); 3]; 8] = [
+    [(-1, 0), (-2, 0), (-3, 0)],
+    [(-1, 1), (-2, 2), (-3, 3)],
+    [(0, 1), (0, 2), (0, 3)],
+    [(1, 1), (2, 2), (3, 3)],
+    [(1, 0), (2, 0), (3, 0)],
+    [(1, -1), (2, -2), (3, -3)],
+    [(0, -1), (0, -2), (0, -3)],
+    [(-1, -1), (-2, -2), (-3, -3)],
+];
 
 fn parse_grid(input: &str) -> miette::Result<Vec<Vec<char>>> {
     let mut outer: Vec<Vec<char>> = Vec::new();
@@ -165,40 +64,6 @@ fn parse_grid(input: &str) -> miette::Result<Vec<Vec<char>>> {
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn test_square() -> miette::Result<()> {
-        let input = vec![
-            ['X', 'M', 'A', 'S'],
-            ['M', 'M', '=', 'A'],
-            ['A', 'M', 'A', 'M'],
-            ['S', 'A', 'M', 'S'],
-        ];
-
-        let hits = find_in_square(input, 0, 0)?;
-        let expect = vec![
-            XmasHit {
-                row: 0,
-                col: 0,
-                orient: Orientation::Horizontal,
-                direction: Direction::Normal,
-            },
-            XmasHit {
-                row: 0,
-                col: 0,
-                orient: Orientation::Vertical,
-                direction: Direction::Normal,
-            },
-            XmasHit {
-                row: 0,
-                col: 0,
-                orient: Orientation::Diagonal,
-                direction: Direction::Normal,
-            },
-        ];
-        assert_eq!(hits, expect);
-        Ok(())
-    }
 
     #[test]
     fn test_build_grid() -> miette::Result<()> {
@@ -216,11 +81,16 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        let input = "XMXMAS
-XMSMSA
-MXXAMA
-MXXAMA
-AXMASX";
+        let input = "MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX";
         assert_eq!(process(input)?, "18");
 
         Ok(())
--- 2024/rust/day-04/src/part2.rs
+++ 2024/rust/day-04/src/part2.rs
@@ -1,6 +1,50 @@
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String> {
-    todo!("day 01 - part 2");
+pub fn process(input: &str) -> miette::Result<String> {
+    let mut x_mas_count = 0;
+
+    let grid = parse_grid(input)?;
+
+    for row in 0..grid.len() {
+        for col in 0..grid[0].len() {
+            if grid[row][col] == 'A' {
+                let word: String = DIRECTIONS
+                    .iter()
+                    .map(|pos| {
+                        let x = row as i32 + pos.0;
+                        let y = col as i32 + pos.1;
+                        match grid.get(x as usize) {
+                            Some(r) => r.get(y as usize).unwrap_or(&'.'),
+                            None => &'.',
+                        }
+                    })
+                    .collect();
+                if (word == "MSSM")
+                    || (word == "MMSS")
+                    || (word == "SMMS")
+                    || (word == "MSMS")
+                    || (word == "SMSM")
+                    || (word == "SSMM")
+                {
+                    x_mas_count += 1;
+                }
+            }
+        }
+    }
+
+    Ok(x_mas_count.to_string())
+}
+
+// Patterns
+const DIRECTIONS: [(i32, i32); 4] = [(-1, -1), (-1, 1), (1, 1), (1, -1)];
+
+fn parse_grid(input: &str) -> miette::Result<Vec<Vec<char>>> {
+    let mut outer: Vec<Vec<char>> = Vec::new();
+    for line in input.lines() {
+        let row: Vec<char> = line.chars().collect();
+        outer.push(row);
+    }
+
+    Ok(outer)
 }
 
 #[cfg(test)]
@@ -8,10 +52,33 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_build_grid() -> miette::Result<()> {
+        let input = "XMAS\nSMAS\nAMSA\nXMAS";
+
+        let expect = vec![
+            vec!['X', 'M', 'A', 'S'],
+            vec!['S', 'M', 'A', 'S'],
+            vec!['A', 'M', 'S', 'A'],
+            vec!['X', 'M', 'A', 'S'],
+        ];
+        assert_eq!(parse_grid(input)?, expect);
+        Ok(())
+    }
+
+    #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX";
+        assert_eq!(process(input)?, "9");
+
         Ok(())
     }
 }
